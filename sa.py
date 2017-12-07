7a71c980f6a405c0f2d16d82ed37d6d25ad13573
Thipus <nicolas.lecointre.2@gmail.com>
2017-12-07 15:46:26 +0100

    Update of sa.py ending fonction chapters

--- sa.py
+++ sa.py
@@ -1,1 +1,33 @@
-print ("yo")
+#-*- coding: utf8 -*-
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
+
+# show a random quote
+
+user_answer = input("Tapez entrée pour découvrir une autre citation ou B pour quitter le programme")
+
+if user_answer == "B":
+    pass
+elif user_answer == "C":
+    print("C pas la bonne réponse")
+else:
+    #show another quote
+	pass
+def show_random_quote(my_list):
+	# get a random number
+	quote = my_list[0]
+	return quote
+
+print (show_random_quote(quotes))
