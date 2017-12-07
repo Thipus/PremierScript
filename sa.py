4305d082eb59e415674a7901206cf18f6d42104b
Thipus <nicolas.lecointre.2@gmail.com>
2017-12-07 16:49:12 +0100

    Début des cours bonus python, rajoute de quotes.json

--- sa.py
+++ sa.py
@@ -1,4 +1,7 @@
 #-*- coding: utf8 -*-
+
+import random
+
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
@@ -16,18 +19,24 @@ characters = [
 
 # show a random quote
 
-user_answer = input("Tapez entrée pour découvrir une autre citation ou B pour quitter le programme")
-
-if user_answer == "B":
-    pass
-elif user_answer == "C":
-    print("C pas la bonne réponse")
-else:
-    #show another quote
-	pass
 def show_random_quote(my_list):
-	# get a random number
-	quote = my_list[0]
+	random_numb = random.randint(0, len(my_list) - 1)
+	quote = my_list[random_numb]
 	return quote
 
-print (show_random_quote(quotes))
+def capitalize(character):
+	for character in characters:
+		character.capitalize()
+
+def message(character, quote):
+	capitalize(character)
+	capitalize(quote)
+	return "{} a dit : {}".format(character, quote)
+
+user_answer = input("Tapez entrée pour découvrir une autre citation ou B pour quitter le programme")
+
+while user_answer != "B":
+	print (message(show_random_quote(characters), show_random_quote(quotes)))
+	user_answer = input("Tapez entrée pour découvrir une autre citation ou B pour quitter le programme")
+
+
